0d195d8fe851b6b0f6e9b4a4e8287b77fb0ff3b1
guirigo <guirigo@outlook.es>
2024-07-16 22:22:49 +0200

    All files

---
+++ prim.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Library of data input and output
+
+using namespace std; // Use of namespace std C++
+
+int main() {
+
+    cout << " Hello World"<<endl;
+    cout << " Hello World 2:\n";
+
+    return 0; // End the program
+}
