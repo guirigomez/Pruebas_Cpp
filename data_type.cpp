b21e53adb705d210d824bec4b80c46820d2ec853
guirigo <guirigo@outlook.es>
2024-07-16 22:39:24 +0200

    Add file data_type

---
+++ data_type.cpp
@@ -0,0 +1,21 @@
+// Basic data types C++
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1 = 10;
+    float num2 = 3.14;
+    double num3 = 2.71828;
+    char letter = 'A';
+
+    cout << "Integer data type: " << num1 << endl;
+    cout << "Float data type: " << num2 << endl;
+    cout << "Double data type: " << num3 << endl;
+    cout << "Character data type: " << letter << endl;
+
+
+    return 0;
+}
