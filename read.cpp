8382db7c3e53a424ac5af94e78093cd35aed6b6d
guirigo <guirigo@outlook.es>
2024-07-16 22:54:18 +0200

    Add file read

---
+++ read.cpp
@@ -0,0 +1,19 @@
+// Rading or data input
+
+# include <iostream>
+
+using namespace std;
+
+/*This is an example */
+
+int main() {
+
+    double num1;
+
+    cout << "Digit one number" << endl;
+    cin >> num1;
+
+    cout << "\nThe number is = " << num1 << endl;
+
+    return 0;
+}
